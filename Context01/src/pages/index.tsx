d518b015596279e7b0d362a87862e243d5f1f0f4
anji0114 <anji.tnk.140114@gmail.com>
2023-02-18 09:18:54 +0900

    contextの基礎を学びました

---
+++ Context01/src/pages/index.tsx
@@ -0,0 +1,44 @@
+import type { NextPage } from "next";
+import { Dispatch, SetStateAction, useState } from "react";
+import { Todo } from "src/types";
+
+type Props = {
+  todos: Todo[];
+  setTodos: Dispatch<SetStateAction<Todo[]>>;
+};
+
+const Home: NextPage<Props> = ({todos, setTodos}) => {
+  const toggleIsDone = (id: Todo["id"]) => {
+    setTodos((prevTodos) => {
+      return prevTodos.map((todo) => {
+        if (todo.id === id) {
+          return { ...todo, isDone: !todo.isDone };
+        }
+        return todo;
+      });
+    });
+  };
+
+  return (
+    <div>
+      <h3>TODO一覧</h3>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>
+            <label style={{ fontSize: "2rem" }}>
+              <input
+                type="checkbox"
+                checked={todo.isDone}
+                onChange={() => toggleIsDone(todo.id)}
+                style={{ width: "1.5rem", height: "1.5rem" }}
+              />
+              {todo.text}
+            </label>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Home;
